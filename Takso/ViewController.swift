85a1a92b319cee025b7b8ab9f35ea58f55911bb5
diyorbek-hikmatiullyev <Diyorbek192004@icloud.com>
2025-03-14 16:03:12 +0500

    Initial Commit

---
+++ Takso/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Takso
+//
+//  Created by Diyorbek Xikmatullayev on 14/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
